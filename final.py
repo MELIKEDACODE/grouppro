617d69b06bf1be69d0bc6f71232d4a98523b0157
MELIKEDACODE <huntereggett@ad.weber.edu>
2017-01-11 14:50:47 -0700

    finished the project, updated daily journal

--- final.py
+++ final.py
@@ -29,56 +29,6 @@ def get_images(directory=None):
             pass # do nothing with errors tying to open non-images
     return image_list, file_list
 
-def round_corners(original_image, percent_of_side):
-    """ Rounds the corner of a PIL.Image
-    
-    original_image must be a PIL.Image
-    Returns a new PIL.Image with rounded corners, where
-    0 < percent_of_side < 1
-    is the corner radius as a portion of the shorter dimension of original_image
-    """
-    #set the radius of the rounded corners
-    width, height = original_image.size
-    radius = int(percent_of_side * min(width, height)) # radius in pixels
-    
-    ###
-    #create a mask
-    ###
-    
-    #start with transparent mask
-    rounded_mask = PIL.Image.new('RGBA', (width, height), (127,0,127,0))
-    drawing_layer = PIL.ImageDraw.Draw(rounded_mask)
-    
-    # Overwrite the RGBA values with A=255.
-    # The 127 for RGB values was used merely for visualizing the mask
-    
-    # Draw two rectangles to fill interior with opaqueness
-    drawing_layer.polygon([(radius,0),(width-radius,0),
-                            (width-radius,height),(radius,height)],
-                            fill=(127,0,127,255))
-    drawing_layer.polygon([(0,radius),(width,radius),
-                            (width,height-radius),(0,height-radius)],
-                            fill=(127,0,127,255))
-
-    #Draw four filled circles of opaqueness
-    drawing_layer.ellipse((0,0, 2*radius, 2*radius), 
-                            fill=(0,127,127,0)) #top left
-    drawing_layer.ellipse((width-2*radius, 0, width,2*radius), 
-                            fill=(0,127,127,0)) #top right
-    drawing_layer.ellipse((0,height-2*radius,  2*radius,height), 
-                            fill=(0,127,127,0)) #bottom left
-    drawing_layer.ellipse((width-2*radius, height-2*radius, width, height), 
-                            fill=(0,127,127,0)) #bottom right
-                         
-    # Uncomment the following line to show the mask
-    # plt.imshow(rounded_mask)
-    
-    # Make the new image, starting with all transparent
-    result = PIL.Image.new('RGBA', original_image.size, (0,0,0,0))
-    result.paste(original_image, (0,0), mask=rounded_mask)
-    return result
-    
-
 
 #end PLTW functions
 def alter_all_images(directory=None):
@@ -97,23 +47,9 @@ def alter_all_images(directory=None):
     image_list, file_list = get_images(directory)  
     times = 1 #sets counter
     #go through the images and save modified versions
-#start nevan import
-    for n in range(len(image_list)):
-        # Parse the filename
-        filename, filetype = os.path.splitext(file_list[n])
-    
-        new_image = image_list[n].resize((250, 250)) #resize to 250 x250
-        new_image_filename = os.path.join(new_directory, filename + '.png')
-        new_image.save(new_image_filename)
-    directory = os.path.join(directory, 'modified')
-    image_list, file_list = get_images(directory)  
 
-    for n in range(len(image_list)):
-        filename, filetype = os.path.splitext(file_list[n])
-        new_image = round_corners(image_list[n],.50)
-        new_image_filename = os.path.join(new_directory, filename + '.png')
-        new_image.save(new_image_filename)  
-    image_list, file_list = get_images(directory)  
+#start nevan import
+
 
 #end nevan Import
     for n in range(len(image_list)):
@@ -150,34 +86,37 @@ def paste(directory = None):
         
         filename, filetype = os.path.splitext(file_list[n]) #parses filename
         width, height = image_list[n].size
-        midx = width /4 #finds x midpoint
-        midy = height /4#finds y midpoint
+        widthb, heightb = image_list[0].size
         big = image_list[n]#takes current image
         if dep == 0:#checks for first runtime
+            try:
+                pwid, phei = image_list[n+1].size
+            except IndexError:
+                pass
+            midx = (width /2)-(pwid /2) #finds x midpoint
+            midy = (height /2)-(phei /2)#finds y midpoint
             small = image_list[n+1]#takes next image
             big.paste(small, (midx,midy)) #pastes on top
             do_filename = os.path.join(new_directory, filename + '.png')
             big.save(do_filename)#saves to cwd
             dep = dep+1
         else:
-            small = 1
-        do_filename = os.path.join(new_directory, filename + '.png')
-        big.save(do_filename)#saves to cwd
+            try:
+                pwid, phei = image_list[n+1].size
+            except IndexError:
+                pass
+            midx = (widthb /2)-(pwid /2) #finds x midpoint
+            midy = (heightb /2)-(phei /2)#finds y midpoint
+            big =PIL.Image.open(do_filename)
+            try:
+                small = image_list[n+1]
+            except IndexError:
+                return "done"
+            big.paste(small, (midx,midy)) #pastes on top
+            do_filename = os.path.join(new_directory, filename + '.png')
+            big.save(do_filename)#saves to cwd
 
-def makecircles():
-    for n in range(len(image_list)):
-        # Parse the filename
-        filename, filetype = os.path.splitext(file_list[n])
-    
-        new_image = image_list[n].resize((250, 250)) #resize to 250 x250
-        new_image_filename = os.path.join(new_directory, filename + '.png')
-        new_image.save(new_image_filename)
-    directory = os.path.join(directory, 'modified')
-    image_list, file_list = get_images(directory)  
-
-    for n in range(len(image_list)):
-        filename, filetype = os.path.splitext(file_list[n])
-        new_image = round_corners(image_list[n],.50)
-        new_image_filename = os.path.join(new_directory, filename + '.png')
-        new_image.save(new_image_filename)    
-        
+def inception():
+    alter_all_images()
+    os.chdir("modified")
+    paste()
